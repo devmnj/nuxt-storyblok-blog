c572211e08892e525565388405114038d88e3f19
manojap <manojap@outlook.com>
2021-06-11 13:45:49 +0530

    .

--- plugins/components.js
+++ plugins/components.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Page from '~/components/storyblock/Page.vue'
-// import Teaser from '~/components/storyblock/Teaser.vue'
-// import Grid from '~/components/storyblock/Grid.vue'
-// import Feature from '~/components/storyblock/Feature.vue'
 import ArticleTeaser from '~/components/storyblock/ArticleTeaser.vue'
 import MiniArticleTeaser from '~/components/storyblock/MiniArticleTeaser.vue'
 import ContentHolder from '~/components/ContentHolder.vue'
@@ -11,9 +8,6 @@ import PostCard from '~/components/PostCard.vue'
 import Staticpost from '~/components/storyblock/Staticpost.vue'
 
 Vue.component('page', Page)
-// Vue.component('teaser', Teaser)
-// Vue.component('grid', Grid)
-// Vue.component('feature', Feature)
 Vue.component('article-teaser', ArticleTeaser)
 Vue.component('mini-article-teaser', MiniArticleTeaser)
 Vue.component('content-holder', ContentHolder)
